01c604619f80c93895b160bd6c5356114a221ab5
travellercoder <guillermobukowski@gmail.com>
2024-05-01 22:35:53 -0300

    se agrega contador de intentos

--- base-juego-adivinanza-js-master/script.js
+++ base-juego-adivinanza-js-master/script.js
@@ -1,16 +1,22 @@
 // Aca el juego selecciona un numero al azar para que adivine el usuario
 
-let numeroAzar = Math.floor(Math.random()*100) + 1 
+let numeroAzar = Math.floor(Math.random() * 100) + 1
 
 let numeroEntrada = document.getElementById('numeroEntrada');
 let mensaje = document.getElementById('mensaje');
+let intento = document.getElementById('intento')
+let intentos = 0
 
 // esta accion se va a ejecutar cuando se toque el boton chequear 
 
-function chequearResultado(){
+function chequearResultado() {
+
+    intentos++
+    intento.textContent = intentos
+
     let numeroIngresado = parseInt(numeroEntrada.value) //! con parse int pasamos el ingreso de string a number
     //esta condicion evalua si que el valor ingresado sea un numero y que sea mayor a 1 y menor a 100, en caso de no serlo imprime el mensaje
-    if (numeroIngresado <1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
+    if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)) {
         mensaje.textContent = 'Por favor ingresa un numero valido entre 1 y 100'
         mensaje.style.color = 'black'
         return
@@ -20,11 +26,11 @@ function chequearResultado(){
     if (numeroIngresado === numeroAzar) {
         mensaje.textContent = 'Felicitaciones! Has adivinado el numero!!'
         mensaje.style.color = 'green';
-        numeroEntrada.disabled = true; 
-    } else if ( numeroIngresado < numeroAzar){
+        numeroEntrada.disabled = true;
+    } else if (numeroIngresado < numeroAzar) {
         mensaje.textContent = 'El Numero es mas alto!'
         mensaje.style.color = 'red';
-    } else { 
+    } else {
         mensaje.textContent = 'El Numero es mas bajo!'
         mensaje.style.color = 'red';
     }
